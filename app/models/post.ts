f54dc9f6028d69c05817a59f03d1484e65bba501
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2024-03-21 11:06:16 -0300

    Using soft-deletes on post

--- app/models/post.ts
+++ app/models/post.ts
@@ -1,7 +1,9 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { compose } from '@adonisjs/core/helpers'
+import { SoftDeletes } from 'adonis-lucid-soft-deletes'
 
-export default class Post extends BaseModel {
+export default class Post extends compose(BaseModel, SoftDeletes) {
   @column({ isPrimary: true })
   declare id: number
 
@@ -16,4 +18,7 @@ export default class Post extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @column.dateTime()
+  declare deletedAt: DateTime
 }
